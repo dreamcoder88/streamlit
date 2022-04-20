1a1496a54f3100cb6326a6917285b1e7f1b19245
dreamcoder88 <70472973+dreamcoder88@users.noreply.github.com>
2022-04-20 06:57:54 +0530

    Add files via upload

---
+++ CryptoChart.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import pandas_datareader as web
+import datetime as dt
+import numpy as np
+import streamlit as st
+st.title('CryptoChart')
+with st.form(key='my_form'):
+    crypto=st.selectbox('Select Cryptocurrency', ['BTC','ETH','XRP','BCH','SOL','BNB','USDT','DOGE','MATIC','USDT','LUNA'])
+    start=st.date_input('Start')
+    end=st.date_input('End')
+    submit_button = st.form_submit_button(label='Submit')
+
+if submit_button:
+    df = web.DataReader(f'{crypto}-USD', 'yahoo', start, end)
+
+    st.line_chart(df['Adj Close'])
